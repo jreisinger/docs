3c655af64257d3e5fcf618149e1e9bc0f12563fa
Jozef Reisinger <jozef.reisinger@pan-net.eu>
2019-12-14 09:36:07 +0100

    homepage simplified

--- main.go
+++ main.go
@@ -2,292 +2,66 @@ package main
 
 import (
 	"html/template"
-	"io/ioutil"
 	"log"
 	"net/http"
-	"os"
-	"path/filepath"
-	"sort"
-	"strings"
-	"time"
+	"path"
 
-	"github.com/gomarkdown/markdown"
-	"github.com/gorilla/handlers"
-	"github.com/gorilla/mux"
-	git "gopkg.in/src-d/go-git.v4"
+	"github.com/jreisinger/homepage/util"
 )
 
 const repoURL = "https://github.com/jreisinger/homepage"
 const repoPath = "/tmp/homepage"
-const dataPath = "/tmp/homepage/data"
 
-func main() {
-	r := mux.NewRouter()
-
-	// Serve static files
-	r.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir(repoPath+"/static/"))))
-	r.HandleFunc("/favicon.ico", func(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile(w, r, repoPath+"/static/favicon.ico")
-	})
-
-	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		t, err := template.New("page").Parse(tplPage)
-		check(err)
-		t.New("head").Parse(tplHead)
-		t.New("navbar").Parse(tplNavbar)
-
-		p := page{Title: "home", RepoURL: repoURL}
-		p.Generate()
-
-		err = t.Execute(w, p)
-		check(err)
-	})
-
-	r.HandleFunc("/{what}", func(w http.ResponseWriter, r *http.Request) {
-		vars := mux.Vars(r)
-		what := vars["what"]
-
-		if isDir(dataPath + "/" + what) {
-			t, err := template.New("index").Parse(tplIndex)
-			check(err)
-			t.New("head").Parse(tplHead)
-			t.New("navbar").Parse(tplNavbar)
-
-			p := index{Title: what, RepoURL: repoURL, Dir: what}
-			p.Generate()
-
-			err = t.Execute(w, p)
-			check(err)
-		} else {
-			t, err := template.New("page").Parse(tplPage)
-			check(err)
-			t.New("head").Parse(tplHead)
-			t.New("navbar").Parse(tplNavbar)
-
-			p := page{Title: what, RepoURL: repoURL}
-			p.Generate()
-
-			err = t.Execute(w, p)
-			check(err)
-		}
-	})
-
-	r.HandleFunc("/{what}/{category}", func(w http.ResponseWriter, r *http.Request) {
-		vars := mux.Vars(r)
-		what := vars["what"]
-		category := vars["category"]
-
-		if isDir(dataPath + "/" + what + "/" + category) {
-			t, err := template.New("index").Parse(tplIndex)
-			check(err)
-			t.New("head").Parse(tplHead)
-			t.New("navbar").Parse(tplNavbar)
-
-			p := index{Title: category, RepoURL: repoURL, Dir: what + "/" + category}
-			p.Generate()
-
-			err = t.Execute(w, p)
-			check(err)
-		} else {
-			t, err := template.New("page").Parse(tplPage)
-			check(err)
-			t.New("head").Parse(tplHead)
-			t.New("navbar").Parse(tplNavbar)
-
-			p := page{Title: category, RepoURL: repoURL, Dir: what}
-			p.Generate()
-
-			err = t.Execute(w, p)
-			check(err)
-		}
-	})
-
-	r.HandleFunc("/{what}/{category}/{item}", func(w http.ResponseWriter, r *http.Request) {
-		vars := mux.Vars(r)
-		what := vars["what"]
-		category := vars["category"]
-		item := vars["item"]
-
-		t, err := template.New("page").Parse(tplPage)
-		check(err)
-		t.New("head").Parse(tplHead)
-		t.New("navbar").Parse(tplNavbar)
-
-		p := page{Title: item, RepoURL: repoURL, Dir: what + "/" + category}
-		p.Generate()
-
-		err = t.Execute(w, p)
-		check(err)
-	})
-
-	go gitPuller()
-
-	loggedRouter := handlers.LoggingHandler(os.Stdout, r)
-	log.Fatal(http.ListenAndServe(":5001", loggedRouter))
-}
-
-func isDir(filename string) bool {
-	fi, err := os.Lstat(filename)
-	if err != nil {
-		return false
-	}
-	return fi.Mode().IsDir()
-}
-
-//
-// Git
-//
-
-func gitClone() {
-	_, err := git.PlainClone(repoPath, false, &git.CloneOptions{
-		URL:      repoURL,
-		Progress: os.Stdout,
-	})
-	check(err)
-}
-
-func gitPull() {
-	// We instantiate a new repository targeting the given path (the .git folder)
-	r, err := git.PlainOpen(repoPath)
-	check(err)
-
-	// Get the working directory for the repository
-	w, err := r.Worktree()
-	check(err)
-
-	// Pull the latest changes from the origin remote and merge into the current branch
-	err = w.Pull(&git.PullOptions{RemoteName: "origin"})
-	//check(err)
-	log.Print(err)
-}
-
-func gitPuller() {
-	for {
-		if _, err := os.Stat(repoPath); os.IsNotExist(err) {
-			gitClone()
-		}
-
-		gitPull()
-		time.Sleep(time.Second * 2)
-	}
-}
-
-// check errors
-func check(err error) {
-	if err != nil {
-		log.Fatal(err)
-	}
-}
-
-//
-// Index
-//
-
-type index struct {
-	Title   string
-	Items   []string
-	Err     error
-	RepoURL string
-	Dir     string
-}
-
-func (i *index) Generate() {
-	dir, err := os.Open(dataPath + "/" + i.Dir)
-	if err != nil {
-		i.Err = err
-		return
-	}
-	defer dir.Close()
-
-	fileInfos, err := dir.Readdir(-1) // -1 means return all entries
-	if err != nil {
-		i.Err = err
-		return
-	}
-	var dirs []string
-	for _, fi := range fileInfos {
-		basename := fi.Name()
-		name := strings.TrimSuffix(basename, filepath.Ext(basename))
-		dirs = append(dirs, name)
-	}
-
-	sort.Strings(dirs)
-	i.Items = dirs
-}
-
-//
-// Page
-//
-
-type page struct {
+type Page struct {
 	Title   string
 	Body    template.HTML
-	Err     error
 	RepoURL string
-	Dir     string
+	IsDir   bool
+	Files   []string
+	UrlPath string
 }
 
-func (p *page) Generate() {
-	md, err := ioutil.ReadFile(dataPath + "/" + p.Dir + "/" + p.Title + ".md")
-	if err != nil {
-		p.Err = err
+func renderPage(urlPath string) (*Page, error) {
+	dataPath := repoPath + "/data"
+
+	// map URL path to filesystem path (without .md)
+	filePath := dataPath + "/" + urlPath[1:] // remove leading /
+
+	title := path.Base(filePath)
+
+	if util.IsDir(filePath) {
+		// if file is a dir return list of files within dir
+		files := util.ListFiles(filePath)
+		return &Page{Title: title, Files: files, RepoURL: repoURL, UrlPath: urlPath, IsDir: true}, nil
+	} else if util.IsFile(filePath + ".md") {
+		// if file is a file return file contents
+		body := util.MdToHtml(filePath + ".md")
+		return &Page{Title: title, Body: body, RepoURL: repoURL, UrlPath: urlPath}, nil
 	}
 
-	html := markdown.ToHTML(md, nil, nil)
-	p.Body = template.HTML(html)
+	// FIXME: if filePath does not map to en existing directory nor file return error
+	return nil, nil
 }
 
-//
-// Templates for web pages.
-//
+// handle all requests
+func handler(w http.ResponseWriter, r *http.Request) {
+	urlPath := r.URL.Path
 
-const tplHead = `
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>{{.Title}}</title>
-	<link rel="stylesheet" type="text/css" href="/static/css/style.css">
-`
+	if urlPath == "/" {
+		http.Redirect(w, r, "/about", http.StatusFound)
+	}
 
-const tplNavbar = `
-	<a href="/">home</a> |
-	<a href="/notes">notes</a> |
-	<a href="/blog">blog</a> |
-	<a href="/temp">temp</a>
-`
+	p, err := renderPage(urlPath)
+	util.Check(err)
 
-const tplIndex = `
-<!DOCTYPE html>
-<html>
-	<head>
-		{{ template "head" }}
-	</head>
-	<body>
-		{{ template "navbar" }}
-		> <u>{{.Dir}}</u>
-		{{$Dir:=.Dir}}
-		<ul>
-		{{range .Items}}
-			<li><a href="/{{ $Dir }}/{{ . }}">{{ . }}</a></li>
-		{{end}}
-		</ul>
-		<a href="{{.RepoURL}}/tree/master/data/{{$Dir}}">source</a>
-	</body>
-</html>`
+	t, err := template.ParseFiles("template/page.html")
+	util.Check(err)
 
-const tplPage = `
-<!DOCTYPE html>
-<html>
-	<head>
-		{{ template "head" }}
-	</head>
-	<body>
-		{{ template "navbar" }}
-		{{if .Dir}}
-		> <u>{{.Dir}}/{{.Title}}</u>
-		{{else}}
-		> <u>{{.Title}}</u>
-		{{end}}
-        {{.Body}}
-		<a href="{{.RepoURL}}/tree/master/data/{{.Dir}}/{{.Title}}.md">source</a>
-	</body>
-</html>`
+	t.Execute(w, p)
+}
+
+func main() {
+	http.HandleFunc("/", handler)
+	go util.GitPuller(repoURL, repoPath)
+	log.Fatal(http.ListenAndServe(":5001", nil))
+}
---
+++ util/git.go
@@ -0,0 +1,44 @@
+package util
+
+import (
+	"log"
+	"os"
+	"time"
+
+	git "gopkg.in/src-d/go-git.v4"
+)
+
+func gitClone(repoURL string, repoPath string) {
+	_, err := git.PlainClone(repoPath, false, &git.CloneOptions{
+		URL:      repoURL,
+		Progress: os.Stdout,
+	})
+	Check(err)
+}
+
+func gitPull(repoURL string, repoPath string) {
+	// We instantiate a new repository targeting the given path (the .git folder)
+	r, err := git.PlainOpen(repoPath)
+	Check(err)
+
+	// Get the working directory for the repository
+	w, err := r.Worktree()
+	Check(err)
+
+	// Pull the latest changes from the origin remote and merge into the current branch
+	err = w.Pull(&git.PullOptions{RemoteName: "origin"})
+	//check(err)
+	log.Print(err)
+}
+
+// GitPuller regularly git pulls a repoURL to repoPath. Can be run as a goroutine.
+func GitPuller(repoURL string, repoPath string) {
+	for {
+		if _, err := os.Stat(repoPath); os.IsNotExist(err) {
+			gitClone(repoURL, repoPath)
+		}
+
+		gitPull(repoURL, repoPath)
+		time.Sleep(time.Second * 2)
+	}
+}
