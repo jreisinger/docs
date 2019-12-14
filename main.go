73fcd70a4b9fb24bacbfcb0a8fe5e8876d629563
Jozef Reisinger <jozef.reisinger@pan-net.eu>
2019-12-14 10:51:18 +0100

    update files main.go, template/page.html

--- main.go
+++ main.go
@@ -12,12 +12,22 @@ const repoURL = "https://github.com/jreisinger/homepage"
 const repoPath = "/tmp/homepage"
 
 func main() {
+	// serve static files
+	// (https://www.alexedwards.net/blog/serving-static-sites-with-go)
+	fileServer := http.FileServer(http.Dir(repoPath + "/static/"))
+	http.Handle("/static/", http.StripPrefix("/static/", fileServer))
+
+	// serve the rest
 	http.HandleFunc("/", handler)
+
+	// regularly update the local repo from the upstream repo
 	go util.GitPuller(repoURL, repoPath)
+
+	// start a webserver
 	log.Fatal(http.ListenAndServe(":5001", nil))
 }
 
-// handle all requests
+// handle requests
 func handler(w http.ResponseWriter, r *http.Request) {
 	urlPath := r.URL.Path
 
