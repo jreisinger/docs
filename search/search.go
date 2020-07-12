b312a289ce1b03f93cf5f76517f6424f306effa6
Jozef Reisinger <jozef.reisinger@pan-net.eu>
2020-07-12 18:00:00 +0200

    move search functions to separate package

---
+++ search/search.go
@@ -0,0 +1,43 @@
+package search
+
+import (
+	"regexp"
+	"strings"
+)
+
+func What(pattern string) bool {
+	var searchOnlyPath bool
+	if strings.HasPrefix(pattern, "path:") {
+		searchOnlyPath = true
+		pattern = strings.TrimPrefix(pattern, "path:")
+	}
+	return searchOnlyPath
+}
+
+// GrepFileContent searches for pattern inside inside a file.
+func GrepFileContent(content string, pattern *regexp.Regexp) bool {
+	return pattern.Match([]byte(content))
+}
+
+// GrepFilePath searches for pattern inside a file pathh.
+func GrepFilePath(path string, pattern *regexp.Regexp) bool {
+	match := pattern.FindString(path)
+	if match == "" {
+		return false
+	}
+	return true
+}
+
+// FilesystemToURL changes filesystem path into URL path.
+func FilesystemToURL(path string) (string, error) {
+	// Trim .md suffix
+	path = strings.TrimSuffix(path, ".md")
+
+	// Trim .*data/ prefix
+	rx, err := regexp.Compile(`.*data/`)
+	if err != nil {
+		return path, err
+	}
+	path = rx.ReplaceAllString(path, "")
+	return path, nil
+}
--- util/util.go
+++ util/util.go
@@ -6,7 +6,6 @@ import (
 	"log"
 	"os"
 	"path/filepath"
-	"regexp"
 	"sort"
 	"strings"
 
@@ -83,32 +82,4 @@ func RemoveTralingSlash(s string) string {
 		s = s[:len(s)-len(suffix)]
 	}
 	return s
-}
-
-// GrepFileContent searches for pattern inside inside a file.
-func GrepFileContent(content string, pattern *regexp.Regexp) bool {
-	return pattern.Match([]byte(content))
-}
-
-// GrepFilePath searches for pattern inside a file pathh.
-func GrepFilePath(path string, pattern *regexp.Regexp) bool {
-	match := pattern.FindString(path)
-	if match == "" {
-		return false
-	}
-	return true
-}
-
-// FilesystemToURL changes filesystem path into URL path.
-func FilesystemToURL(path string) (string, error) {
-	// Trim .md suffix
-	path = strings.TrimSuffix(path, ".md")
-
-	// Trim .*data/ prefix
-	rx, err := regexp.Compile(`.*data/`)
-	if err != nil {
-		return path, err
-	}
-	path = rx.ReplaceAllString(path, "")
-	return path, nil
 }
