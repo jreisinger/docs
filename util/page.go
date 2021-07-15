4dc30c5c55d0a1abf65f699e6092d629c9a2b4e9
Jozef Reisinger <jozef.reisinger@pan-net.eu>
2021-07-15 21:21:19 +0200

    remove titleCasePathComponents function

--- util/page.go
+++ util/page.go
@@ -31,58 +31,28 @@ func RenderPage(repoURL string, repoPath string, urlPath string) (*Page, error) 
 
 	title := path.Base(filePath)
 
-	// generate filePath variants with Title cased components
-	var allFilePathVariants []string
-	allFilePathVariants = append(allFilePathVariants, filePath)
-	allFilePathVariants = append(allFilePathVariants, titleCasePathComponents(filePath)...)
-
-	for _, filePath := range allFilePathVariants {
-
-		if IsDir(filePath) { // if file is a dir return list of files within dir
-			files := ListFiles(filePath, false)
-			if strings.HasSuffix(filePath, "blog") {
-				files = ListFiles(filePath, true)
-			}
-			return &Page{Title: title, Files: files, RepoURL: repoURL, UrlPath: urlPath, IsDir: true}, nil
-		} else if IsFile(filePath + ".md") { // if file is a file return file contents
-			lastModified, err := LastModified(repoPath, filePath+".md")
-			if err != nil {
-				return nil, err
-			}
-
-			data, err := ioutil.ReadFile(filePath + ".md")
-			if err != nil {
-				return nil, err
-			}
-
-			body := MdToHtml(data)
-
-			return &Page{Title: title, Body: body, RepoURL: repoURL, UrlPath: urlPath, LastModified: lastModified}, nil
+	if IsDir(filePath) { // if file is a dir return list of files within dir
+		files := ListFiles(filePath, false)
+		if strings.HasSuffix(filePath, "blog") {
+			files = ListFiles(filePath, true)
+		}
+		return &Page{Title: title, Files: files, RepoURL: repoURL, UrlPath: urlPath, IsDir: true}, nil
+	} else if IsFile(filePath + ".md") { // if file is a file return file contents
+		lastModified, err := LastModified(repoPath, filePath+".md")
+		if err != nil {
+			return nil, err
 		}
 
+		data, err := ioutil.ReadFile(filePath + ".md")
+		if err != nil {
+			return nil, err
+		}
+
+		body := MdToHtml(data)
+
+		return &Page{Title: title, Body: body, RepoURL: repoURL, UrlPath: urlPath, LastModified: lastModified}, nil
 	}
 
 	err := ErrorNotFound
 	return nil, err
-}
-
-// titleCasePathComponents returns title paths with Title cased components.
-func titleCasePathComponents(filePath string) []string {
-	components := strings.Split(filePath, "/")
-	var titleCaseFilePath []string
-	for i := range components {
-		before := strings.Join(components[:i], "/")
-		titleCased := strings.Title(components[i])
-		after := strings.Join(components[i+1:], "/")
-		// fmt.Printf("%d: %v, %v, %v\n", i, before, titleCased, after)
-		var newComponents []string
-		for _, c := range []string{before, titleCased, after} {
-			if c != "" {
-				newComponents = append(newComponents, c)
-			}
-		}
-		p := strings.Join(newComponents, "/")
-		titleCaseFilePath = append(titleCaseFilePath, p)
-	}
-	return titleCaseFilePath[1:]
 }
