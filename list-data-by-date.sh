aaccdae1b110262393b18a9ef852c702e53f9fa1
Jozef Reisinger <jozef.reisinger@gmail.com>
2022-06-20 09:26:52 +0200

    sort oldest data last

--- list-data-by-date.sh
+++ list-data-by-date.sh
@@ -2,5 +2,5 @@
 
 git ls-files -z -- "data" | \
 xargs -0 -n1 -I{} -- git log -1 --format="%at {}" {} | \
-sort | \
+sort -r | \
 perl -MTime::Piece -lape 's/(^\d+)/localtime($1)->strftime("%F %T")/e'
