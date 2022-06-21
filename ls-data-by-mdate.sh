98ee2395df8ca01f334547375a508f589b4417b4
Jozef Reisinger <jozef.reisinger@gmail.com>
2022-06-21 09:12:30 +0200

    head and tail

--- ls-data-by-mdate.sh
+++ ls-data-by-mdate.sh
@@ -3,4 +3,5 @@
 git ls-files -z -- "data" | \
 xargs -0 -n1 -I{} -- git log -1 --format="%at {}" {} | \
 sort -r | \
-perl -MTime::Piece -lape 's/(^\d+)/localtime($1)->strftime("%F")/e'
+perl -MTime::Piece -lape 's/(^\d+)/localtime($1)->strftime("%F")/e' | \
+(head; echo "..."; tail)
