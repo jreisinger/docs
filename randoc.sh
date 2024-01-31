cce52b013a105da266a45a3ed776505541dc403e
jozef <jozef.reisinger@gmail.com>
2024-01-31 14:59:33 +0100

    get only .md files

--- randoc.sh
+++ randoc.sh
@@ -2,5 +2,5 @@
 
 NDOCS=$(find . -type f -iname '*.md' | wc -l)
 N=$(shuf -i 1-"$NDOCS" -n 1)
-DOC=$(find . -type f | sed -n "${N}"p)
-echo "$DOC"
+DOC=$(find . -type f -iname '*.md' | sed -n "${N}"p)
+echo "$DOC (out of $NDOCS)"
